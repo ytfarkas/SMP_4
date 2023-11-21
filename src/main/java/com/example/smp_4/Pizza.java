cdc7493d9d607b94a24daa97a7015a7e9916cdf4
ytfarkas <ytfarkas@gmail.com>
2023-11-20 22:23:16 -0500

    this is most likely the wrong way to do this

--- src/main/java/com/example/smp_4/Pizza.java
+++ src/main/java/com/example/smp_4/Pizza.java
@@ -9,4 +9,27 @@ public abstract class Pizza {
     protected boolean extraSauce;
     protected boolean extraCheese;
     public abstract double price(); //polymorphism
+
+    public void setToppingsList(){
+        toppings = new ArrayList<>();
+    }
+    public void setSize(Size pizzaSize){
+        size = pizzaSize;
+    }
+
+    public void setSauce(Sauce pizzaSauce){
+        sauce = pizzaSauce;
+    }
+
+    public void setExtraSauce(Boolean bool){
+        extraSauce = bool;
+    }
+    public void setExtraCheese(Boolean bool){
+        extraCheese = bool;
+    }
+
+    public void setToppings(Topping topping){
+        toppings.add(topping);
+    }
+
 }
