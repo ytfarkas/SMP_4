ad3c7ac25d853bff0d018de0f58f11267f5877e6
David Rahabi <davidrahabi@gmail.com>
2023-11-22 01:08:48 -0500

    finished pizza

--- src/main/java/com/example/smp_4/Order.java
+++ src/main/java/com/example/smp_4/Order.java
@@ -1,4 +1,22 @@
 package com.example.smp_4;
+import java.util.UUID;
 
 public class Order {
+    private int id;
+    private Pizza[] orderList;
+
+    public Order(int id, Pizza[] orderList){
+        this.id = id;
+        this.orderList = orderList;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public Pizza[] getOrderList(){
+        return orderList;
+    }
+
+
 }
--- src/main/java/com/example/smp_4/StoreOrders.java
+++ src/main/java/com/example/smp_4/StoreOrders.java
@@ -1,4 +1,23 @@
 package com.example.smp_4;
 
+import java.util.ArrayList;
+
 public class StoreOrders {
+    private ArrayList<Order> storeOrderList;
+    private static int nextOrderNumber;
+
+    public StoreOrders(){
+        storeOrderList = new ArrayList<Order>();
+        nextOrderNumber = 0;
+    }
+
+    public void addOrder(Order order){
+        storeOrderList.add(order);
+    }
+    public int getNextOrderNumber(){
+        return nextOrderNumber;
+    }
+    public ArrayList<Order> getStoreOrderList() {
+        return storeOrderList;
+    }
 }
